fb0af240f0fe51d38e3e8c8a32b0c95de63dba68
Jon Hancock <jhancock@shellshadow.com>
2019-11-10 05:58:47 +0000

    transfered to new server.  basic install working

--- notes/nginx/copy_nginx.sh
+++ notes/nginx/copy_nginx.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo cp /home/lov/lov/notes/nginx/nginx.conf /etc/nginx/.
-sudo cp /home/lov/lov/notes/nginx/lov_is.conf /etc/nginx/conf.d/.
+sudo cp /home/dev/lov/notes/nginx/nginx.conf /etc/nginx/.
+sudo cp /home/dev/lov/notes/nginx/lov_is.conf /etc/nginx/conf.d/.
 sudo /usr/sbin/nginx -t
 sudo service nginx restart
