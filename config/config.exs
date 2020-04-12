22d803e9e5f651b1b497a7bf63c1ff33eef75236
Jon Hancock <jhancock@shellshadow.com>
2020-04-12 01:25:03 +0000

    fixed issues with tailwind and webpack...i think.  app seems to be working

--- config/config.exs
+++ config/config.exs
@@ -15,7 +15,7 @@ config :lov, LovWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "PrDtXiz379f5DX2KmadzVKfOHQj2/ypP/Y2mnjEZOBbWd79YCw82x6mbZ0619Q41",
   render_errors: [view: LovWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Lov.PubSub, adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Lov.PubSub, adapter: Phoenix.PubSub.PG2],
   live_view: [signing_salt: "FWjq0Sj6"]
 
 # Configures Elixir's Logger
