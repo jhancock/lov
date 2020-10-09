f740d5b09279174bd31f2ef0e9a0fd32fb275cc8
Jon Hancock <jon@coreschedule.com>
2020-10-09 13:25:01 +1300

    upgraded to phx 1.5.5.  skipped upgrades to changes in webpack config as I have webpack somewhat stable

--- mix.exs
+++ mix.exs
@@ -33,20 +33,20 @@ defmodule Lov.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.1"},
+      {:phoenix, "~> 1.5.5"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:phoenix_live_dashboard, "~> 0.2"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.2.0"},
-      {:mojito, "~> 0.6.1"},
-      {:mogrify, "~> 0.7.3"}
+      {:mojito, "~> 0.7.3"},
+      {:mogrify, "~> 0.8.0"}
     ]
   end
 
@@ -61,7 +61,7 @@ defmodule Lov.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
   end
 end
